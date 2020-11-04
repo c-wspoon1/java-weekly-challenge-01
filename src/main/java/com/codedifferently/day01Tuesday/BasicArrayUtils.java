29b2bef0f84da0c430e364f628a0154d2795c268
Corban Weatherspoon <corban.wspoon@gmail.com>
2020-11-03 20:50:19 -0500

    completedTuesdayJavaChallenge

--- src/main/java/com/codedifferently/day01Tuesday/BasicArrayUtils.java
+++ src/main/java/com/codedifferently/day01Tuesday/BasicArrayUtils.java
@@ -1,12 +1,15 @@
 package com.codedifferently.day01Tuesday;
 
+import java.sql.SQLOutput;
+
 public class BasicArrayUtils {
     /**
      * @param stringArray an array of String objects
      * @return the first element in the array
      */
     public static String getFirstElement(String[] stringArray) {
-        return null;
+        String firstElement = stringArray[0];
+        return firstElement;
     }
 
     /**
@@ -14,7 +17,7 @@ public class BasicArrayUtils {
      * @return the second element in the array
      */
     public static String getSecondElement(String[] stringArray) {
-        return null;
+        return stringArray[1];
     }
 
     /**
@@ -22,14 +25,29 @@ public class BasicArrayUtils {
      * @return stringArray with the elements in reverse order
      */
     public static String[] reverse(String[] stringArray) {
-        return null;
-    }
+        String[] rev =new String[stringArray.length];
+
+        int counter = 0;
+
+        for(int index = stringArray.length - 1; index >= 0; index--) {
+            rev[counter] = stringArray[index];
+        counter++;
+            System.out.println("index: " + index);
+            System.out.println("stringArray[" + index + "]: " + stringArray[index]);
+        }
+        return rev;
+   }
 
     /**
      * @param stringArray an array of String objects
      * @return String made up of the first character in each element of stringArray
      */
     public static String getFirstLetterOfEachElement(String[] stringArray) {
-        return null;
+        String firstLetter = "";
+        for(int i = 0; i < stringArray.length; i++){
+            firstLetter += stringArray[i].charAt(0) +  "";
+
+        }
+        return firstLetter;
     }
 }
